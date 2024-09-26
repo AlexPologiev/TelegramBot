8c466e20079c14cbff738a644bade6878d203976
Alexandr Polozhiev <A_pologiev@mail.ru>
2024-09-26 20:15:20 +0300

    add new Subscriber id db.

--- src/main/java/com/skillbox/cryptobot/bot/command/StartCommand.java
+++ src/main/java/com/skillbox/cryptobot/bot/command/StartCommand.java
@@ -1,11 +1,14 @@
 package com.skillbox.cryptobot.bot.command;
 
+import com.skillbox.cryptobot.model.Subscribers;
+import com.skillbox.cryptobot.repository.SubscribersRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.extensions.bots.commandbot.commands.IBotCommand;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.bots.AbsSender;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
@@ -18,6 +21,7 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 @Slf4j
 public class StartCommand implements IBotCommand {
 
+    private final SubscribersRepository repository;
     @Override
     public String getCommandIdentifier() {
         return "start";
@@ -31,6 +35,20 @@ public class StartCommand implements IBotCommand {
     @Override
     public void processMessage(AbsSender absSender, Message message, String[] arguments) {
         SendMessage answer = new SendMessage();
+
+        User user = message.getFrom();
+        Long userId = user.getId();;
+        Subscribers subscriber = repository.findSubscribersByUserId(userId);
+
+        if (subscriber == null){
+            Subscribers newSubscriber = new Subscribers();
+            newSubscriber.setUserId(userId);
+            repository.save(newSubscriber);
+            log.info("New user has been saved in db with userID: " + userId);
+        } else {
+            log.info("User with userID: " + userId + " already exists");
+        }
+
         answer.setChatId(message.getChatId());
 
         answer.setText("""
--- src/main/java/com/skillbox/cryptobot/repository/SubscribersRepository.java
+++ src/main/java/com/skillbox/cryptobot/repository/SubscribersRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SubscribersRepository extends JpaRepository<Subscribers, Long> {
+
+    Subscribers findSubscribersByUserId(Long id);
 }
