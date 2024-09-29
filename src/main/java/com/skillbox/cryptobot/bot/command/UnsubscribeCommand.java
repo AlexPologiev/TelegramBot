316ecb285efefd039a032be61a6613436b88f814
Alexandr Polozhiev <A_pologiev@mail.ru>
2024-09-29 16:46:41 +0300

    Deleting active subscribe

--- src/main/java/com/skillbox/cryptobot/bot/command/UnsubscribeCommand.java
+++ src/main/java/com/skillbox/cryptobot/bot/command/UnsubscribeCommand.java
@@ -1,10 +1,14 @@
 package com.skillbox.cryptobot.bot.command;
 
+import com.skillbox.cryptobot.model.Subscribers;
+import com.skillbox.cryptobot.repository.SubscribersRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.extensions.bots.commandbot.commands.IBotCommand;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.bots.AbsSender;
 
 /**
@@ -15,6 +19,7 @@ import org.telegram.telegrambots.meta.bots.AbsSender;
 @AllArgsConstructor
 public class UnsubscribeCommand implements IBotCommand {
 
+    private final SubscribersRepository repository;
 
     @Override
     public String getCommandIdentifier() {
@@ -28,6 +33,24 @@ public class UnsubscribeCommand implements IBotCommand {
 
     @Override
     public void processMessage(AbsSender absSender, Message message, String[] arguments) {
+        User user = message.getFrom();
+        Long userId = user.getId();
 
+        Subscribers subscriber = repository.findSubscribersByUserId(userId);
+
+        if (subscriber != null) {
+            subscriber.setPrice(null);
+            repository.save(subscriber);
+        }
+
+
+        SendMessage answer = new SendMessage();
+        answer.setChatId(message.getChatId());
+        try {
+            answer.setText("Подписка отменена");
+            absSender.execute(answer);
+        } catch (Exception e) {
+            log.error("Ошибка возникла в /unsubscribe методе", e);
+        }
     }
 }
