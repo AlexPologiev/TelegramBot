49d875d0d86bdfa735e672ff47ee20d5bf7529a8
Alexandr Polozhiev <A_pologiev@mail.ru>
2024-10-07 22:00:49 +0300

    create sendMessage service

--- src/main/java/com/skillbox/cryptobot/bot/CryptoBot.java
+++ src/main/java/com/skillbox/cryptobot/bot/CryptoBot.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
 import org.telegram.telegrambots.extensions.bots.commandbot.commands.IBotCommand;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.List;
 
@@ -36,5 +38,16 @@ public class CryptoBot extends TelegramLongPollingCommandBot {
     @Override
     public void processNonCommandUpdate(Update update) {
         System.out.println("hello");
+    }
+
+    public void sendMessage(Long chatId, String textToSend) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(textToSend);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            log.info(e.getMessage());
+        }
     }
 }
